824d6d71d4b17c43012fe4c62e630b9fc09484e0
btanmay <budagetanmay@gmail.com>
2021-01-26 18:20:05 +0530

    Initial commit day1 signin signup sign out using firebase realtime database

---
+++ app/src/main/java/com/example/whatsappclone/SignUpActivity.java
@@ -0,0 +1,86 @@
+package com.example.whatsappclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.TokenWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.whatsappclone.Models.Users;
+import com.example.whatsappclone.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;                                 //loading dialog box
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        progressDialog = new ProgressDialog(SignUpActivity.this);         //loading dialog box
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating your account");
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();                                            //loading displayed
+
+                //get email and password from user
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString() , binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progressDialog.dismiss();                      //loading screen turn off
+
+                        //binding the data
+                        if (task.isSuccessful()){
+                            Users user = new Users(binding.etUserName.getText().toString(), binding.etEmail.getText().toString() ,
+                                    binding.etPassword.getText().toString());
+
+                            //firebase Real Time Database
+                            String id = task.getResult().getUser().getUid();                //get user id from auth
+                            database.getReference().child("Users").child(id).setValue(user);  //create child nodes(in RTDB)in Users node
+
+                            Toast.makeText(SignUpActivity.this,"User created successfully",Toast.LENGTH_SHORT).show();
+                            
+                        }else {
+                            Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        binding.tvAlreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this , SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
